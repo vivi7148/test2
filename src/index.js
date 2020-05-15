49b457d3f479c5401af5eda3bc1d077f8187dc32
vivi7148 <61859585+vivi7148@users.noreply.github.com>
2020-05-15 23:50:00 +1000

    f

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import App from "./App";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
